e2f6f2b6e0be60267a1f161b24e6cf4998cdb646
John Letey <johnletey@gmail.com>
2021-09-17 22:45:42 +0100

    feat: send a message to Discord

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ config();
 
 // Import dependencies.
 import { ethers } from "ethers";
+import Discord from "webhook-discord";
 
 // Define constants.
 const ADDRESS = "0x8499c62EC0B4F1f2F0eD93e67e19587F6b94afaF";
@@ -24,7 +25,25 @@ provider.resetEventsBlock(13230249);
 
 const contract = new ethers.Contract(ADDRESS, ABI, provider);
 
+// Setup Discord instance.
+const discord = new Discord.Webhook(process.env.DISCORD?.toString()!);
+
 // Listen to new events.
-contract.on("BurnMintToken", (from, event) => {
-  console.log(from);
+contract.on("BurnMintToken", async (from, event) => {
+  const name = (await provider.lookupAddress(from)) || from;
+  
+  console.log(`\naddress = ${name}\ntx      = ${event.transactionHash}\n`);
+
+  const message = new Discord.MessageBuilder()
+    .setColor("#bdff00")
+    .setFooter(
+      "$CITIZEN // kong.land",
+      "https://ipfs.io/ipfs/QmQmZNp7JNdvYAA8ichVr5bVZUUTfU83zJ8hTZoQfb9YBh"
+    )
+    .setTitle(":fire::hammer::fire: New Citizen Forged :fire::hammer::fire:")
+    .setDescription(`Token ID #XXX\n\n${name}`)
+    .setURL(`https://etherscan.io/tx/${event.transactionHash}`)
+    .setName("Hephaestus");
+
+  await discord.send(message);
 });
